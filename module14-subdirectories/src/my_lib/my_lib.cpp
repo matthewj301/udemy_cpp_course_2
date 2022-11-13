8e40e58d155394d1b8b27c724d48d81fd3e84d18
mjohnson <matthewj301@gmail.com>
2022-11-13 16:09:19 -0600

    Initial commit

---
+++ module14-subdirectories/src/my_lib/my_lib.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Matthew Johnson on 10/29/22.
+//
+#include <iostream>
+#include "my_lib.h"
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ module15-variables/src/my_lib/my_lib.h
@@ -0,0 +1,11 @@
+//
+// Created by Matthew Johnson on 10/29/22.
+//
+
+#ifndef UDEMY_CPP_COURSE_2_MY_LIB_H
+#define UDEMY_CPP_COURSE_2_MY_LIB_H
+#pragma once
+
+void hello();
+
+#endif //UDEMY_CPP_COURSE_2_MY_LIB_H
